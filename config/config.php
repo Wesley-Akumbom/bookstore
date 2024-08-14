0abf9e54e7d65a210cb6159c6c5e3c9b43ac5e4d
Akumbom Wesley <akumbomwesley7@gmail.com>
2024-08-14 15:02:05 +0100

    configure database

--- config/config.php
+++ config/config.php
@@ -18,3 +18,9 @@ $pass = '';
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
 
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+if ($conn){
+    echo "db connected successfully";
+} else {
+    echo "error in db connection";
+}
