405e93478064c3b688b1b0e372eae63bd80103ad
Akumbom Wesley <akumbomwesley7@gmail.com>
2024-08-16 11:55:55 +0100

    Implement adding products to cart

--- shopping/single.php
+++ shopping/single.php
@@ -4,6 +4,31 @@
 
 <?php 
 
+    if(isset($_POST['submit'])){
+
+        $pro_id = $_POST['pro_id'];
+        $pro_name = $_POST['pro_name'];
+        $pro_image = $_POST['pro_image'];
+        $pro_price = $_POST['pro_price'];
+        $pro_quantity = $_POST['pro_quantity'];
+        $pro_file = $_POST['pro_file'];
+        $user_id = $_POST['user_id'];
+        
+        $insert = $conn->prepare("INSERT INTO cart (pro_id, pro_name, pro_image, pro_price, pro_quantity, pro_file, user_id) 
+                                                        VALUES (:pro_id, :pro_name, :pro_image, :pro_price, :pro_quantity, :pro_file, :user_id)");
+  
+        $insert->execute([
+            ':pro_id' => $pro_id,
+            ':pro_name' => $pro_name,
+            ':pro_image' => $pro_image,
+            ':pro_price' => $pro_price,
+            ':pro_quantity' => $pro_quantity,
+            ':pro_file' => $pro_file,
+            ':user_id'=> $user_id
+        ]);
+
+}
+
     if(isset($_GET['id'])){
         $id = $_GET['id']; 
 
@@ -37,8 +62,32 @@
                                     </div>
                                 </div>
                                 <p class="about"><?php echo $product->description; ?></p>
-                              
-                                <div class="cart mt-4 align-items-center"> <button class="btn btn-primary text-uppercase mr-2 px-4"><i class="fas fa-shopping-cart"></i> Add to cart</button> </div>
+                                <form method="POST" id="form-data">
+                                    <div class="">
+                                        <input type="text" name="pro_id" value="<?php echo $product->id; ?>" class="form-control">
+                                    </div>
+                                    <div class="">
+                                        <input type="text" name="pro_name" value="<?php echo $product->name; ?>" class="form-control">
+                                    </div>
+                                    <div class="">
+                                        <input type="text" name="pro_image" value="<?php echo $product->image; ?>" class="form-control">
+                                    </div>
+                                    <div class="">
+                                        <input type="text" name="pro_price" value="<?php echo $product->price; ?>" class="form-control">
+                                    </div>
+                                    <div class="">
+                                        <input type="text" name="pro_quantity" value="1" class="form-control">
+                                    </div>
+                                    <div class="">
+                                        <input type="text" name="pro_file" value="<?php echo $product->file; ?>" class="form-control">
+                                    </div>
+                                    <div class="">
+                                        <input type="text" name="user_id" value="<?php echo $_SESSION['user_id']; ?>" class="form-control">
+                                    </div>
+                                    <div class = "cart mt-4 aligin-items-cente">
+                                    <button name="submit" type="submit" class="btn btn-primary text-uppercase mr-2 px-4"><i class="fas fa-shopping-cart"></i> Add to cart</button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
@@ -49,3 +98,24 @@
 
 
 <?php require "../includes/footer.php"; ?>
+
+<script>
+    $(document).ready(function(){
+        $(document).on("submit", function(e) {
+
+            e.preventDefault();
+            
+            var formData = $("#form-data").serialize()+'&submit=submit';
+
+            $.ajax({
+                type: "post",
+                url: "single.php?id=<?php echo $id; ?>",
+                data: formData,
+
+                success:function() {
+                    alert("Added to cart succesfully ");
+                }
+            })
+        })
+});
+</script>
