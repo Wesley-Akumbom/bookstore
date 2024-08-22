69243d82a31b7060f2b0efd8900033631fb801ad
Akumbom Wesley <akumbomwesley7@gmail.com>
2024-08-22 10:26:02 +0100

    Creating admins

--- admin-panel/layouts/header.php
+++ admin-panel/layouts/header.php
@@ -38,13 +38,13 @@
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="<?php echo ADMINURL;?>/admins/admins.html" style="margin-left: 20px;">Admins</a>
+            <a class="nav-link" href="<?php echo ADMINURL;?>/admins/admins.php" style="margin-left: 20px;">Admins</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="<?php echo ADMINURL;?>/categories-admins/show-categories.html" style="margin-left: 20px;">Categories</a>
+            <a class="nav-link" href="<?php echo ADMINURL;?>/categories-admins/show-categories.php" style="margin-left: 20px;">Categories</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="<?php echo ADMINURL;?>/products-admins/show-products.html" style="margin-left: 20px;">Products</a>
+            <a class="nav-link" href="<?php echo ADMINURL;?>/products-admins/show-products.php" style="margin-left: 20px;">Products</a>
           </li>
         
         </ul>
